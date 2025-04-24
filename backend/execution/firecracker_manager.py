217480edc531e684fe447158973938fe59aed26b
Nidhi Shekhar <108923912+NidhiShekhar@users.noreply.github.com>
2025-04-24 23:05:51 +0530

    Update firecracker_manager.py

--- backend/execution/firecracker_manager.py
+++ backend/execution/firecracker_manager.py
@@ -1,13 +1,15 @@
+# TO-DO please i think i might be over complicating this welp
+
 # Import subprocess / firecracker sdk / OS libraries
 
 # Function: execute_function_in_firecracker(code: str, language: str, timeout: int)
-# - Prepare firecracker microVM image
+# - Prepare firecracker microvm image 
 # - Inject user code into VM
-# - Start microVM execution
+# - Start microvm execution
 # - Capture output / error
 # - Measure execution time
 # - Clean up VM
 # - Return output, execution_time, status(success/failure), error_log (if any)
 
-# Note: This is optional (only if firecracker setup is working)
+# idk im not very sure about firecracker, i could use another microvm
 
